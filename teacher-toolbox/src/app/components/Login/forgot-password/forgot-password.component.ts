14e74ebb46fd9e4b79137dfdac229273445e479c
Justin Roehr <justin.roehr9@gmail.com>
2021-09-16 19:34:14 -0500

    Cleaning up some files

--- teacher-toolbox/src/app/components/Login/forgot-password/forgot-password.component.ts
+++ teacher-toolbox/src/app/components/Login/forgot-password/forgot-password.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from "../../services/auth.service";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
   selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  templateUrl: './forgot-password.component.html'
 })
 export class ForgotPasswordComponent implements OnInit {
 
