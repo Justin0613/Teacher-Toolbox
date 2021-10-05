2c6ae864e3068310fa32465f3c428cc5c1ddacdc
Justin0613 <justin.roehr9@gmail.com>
2021-10-05 11:52:18 -0500

    Refactoring into Modules

--- teacher-toolbox/src/app/app.module.ts
+++ teacher-toolbox/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LoginModule } from './login/login.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -11,10 +12,6 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from 'src/app/components/dashboard/dashboard.component';
-import { SignInComponent } from 'src/app/components/Login/sign-in/sign-in.component';
-import { SignUpComponent } from 'src/app/components/Login/sign-up/sign-up.component';
-import { ForgotPasswordComponent } from 'src/app/components/Login/forgot-password/forgot-password.component';
-import { VerifyEmailComponent } from 'src/app/components/Login/verify-email/verify-email.component';
 import { AuthService } from '././services/auth.service';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { StudentsComponent } from './components/students/students.component';
@@ -24,10 +21,6 @@ import { ClassesComponent } from './components/classes/classes.component';
   declarations: [
     AppComponent,
     DashboardComponent,
-    SignInComponent,
-    SignUpComponent,
-    ForgotPasswordComponent,
-    VerifyEmailComponent,
     NavigationBarComponent,
     StudentsComponent,
     ClassesComponent,
@@ -38,7 +31,8 @@ import { ClassesComponent } from './components/classes/classes.component';
     AngularFirestoreModule,
     AngularFireAuthModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LoginModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
