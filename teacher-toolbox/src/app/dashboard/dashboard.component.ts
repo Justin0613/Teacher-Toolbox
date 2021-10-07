49cafab75b32ad0b645959cd987baa5ab36b8174
Justin0613 <justin.roehr9@gmail.com>
2021-10-07 11:24:50 -0500

    Cleaning up styles

--- teacher-toolbox/src/app/dashboard/dashboard.component.ts
+++ teacher-toolbox/src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,8 @@ import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 
 export class DashboardComponent implements OnInit {
