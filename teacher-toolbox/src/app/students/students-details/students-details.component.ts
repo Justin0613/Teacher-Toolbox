6456f29344afa88af95ad6f6ced6f3c281da0dca
xavien77 <75010389+xavien77@users.noreply.github.com>
2021-11-03 15:52:16 -0500

    student details link
    I made the changes to this file so that when a student is added, there information can be accessed

--- teacher-toolbox/src/app/students/students-details/students-details.component.ts
+++ teacher-toolbox/src/app/students/students-details/students-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, OnChanges, EventEmitter } from '@angular/core';
+import { StudentsService } from 'src/app/services/students.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import Student from 'src/models/student.model';
+//import Student from 'src/models/student.model';
 
 @Component({
   selector: 'app-students-details',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentsDetailsComponent implements OnInit {
 
-  constructor() { }
+  //@Input() student: Student;
+  @Output() refreshList: EventEmitter<any> = new EventEmitter();
+  currentStudent: Student = null;
+
+  StudentId: string = "";
+  student: Student = null;
+
+  viewDate: Date = new Date();
+
+  constructor(private studentService: StudentsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.StudentId = params.get('students_id');
+
+      this.studentService.getSingle(this.StudentId, ((data: Student) => {
+        this.student = data;
+      }));
+    });
   }
 
+  ngOnChanges(): void {
+    this.currentStudent = { ...this.student };
+  }
+
+  // updateStudent(): void {
+  //   const data = {
+  //     firstName: this.currentStudent.firstName,
+  //     lastName: this.currentStudent.lastName,
+      
+  //   };
+
+  //   this.studentService.update(this.currentStudent.id, data)
+  //     .catch(error => window.alert(error));
+  // }
+
+  // deleteClassroom(): void {
+  //   this.studentService.delete(this.currentStudent.id)
+  //     .then(() => {
+  //       this.refreshList.emit();
+  //     })
+  //     .catch(error => window.alert(error));
+  // }
 }
