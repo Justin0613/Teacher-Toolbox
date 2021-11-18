d8fd75c21d00e6a95dda2389a18b01099ef7f478
Nicholas Kleiser <86692735+zziitt123@users.noreply.github.com>
2021-11-18 03:44:01 -0600

    Dark Mode
    Dark Mode finally works
    Desktop changed
    Some font-colors changed per page for visibility

--- teacher-toolbox/src/app/dashboard/dashboard-routing.module.ts
+++ teacher-toolbox/src/app/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DARK_MODE_OPTIONS } from 'angular-dark-mode';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
+  providers: [
+    {
+        provide: DARK_MODE_OPTIONS,
+        useValue: {
+            darkModeClass: 'my-dark-mode',
+            lightModeClass: 'my-light-mode'
+        }
+    }
+],
   exports: [RouterModule]
 })
 export class DashboardRoutingModule { }
--- teacher-toolbox/src/app/dashboard/dashboard.component.ts
+++ teacher-toolbox/src/app/dashboard/dashboard.component.ts
@@ -15,3 +15,4 @@ export class DashboardComponent implements OnInit {
   }
 
 }
+
