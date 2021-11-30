2d98e28e4c92c31987e0a7a5d0adb8e010f54cca
Justin Roehr <justin.roehr9@gmail.com>
2021-11-29 21:22:16 -0600

    Adding files needed for deployment

--- teacher-toolbox/src/environments/environment.prod.ts
+++ teacher-toolbox/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-    production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyBtzOUZHgggub4ZnHicqBshgwutKFe9ghw",
+        authDomain: "teacher-toolbox-36b09.firebaseapp.com",
+        projectId: "teacher-toolbox-36b09",
+        storageBucket: "teacher-toolbox-36b09.appspot.com",
+        messagingSenderId: "655898655040",
+        appId: "1:655898655040:web:d7e1c72f6d8c7167db22a5",
+        measurementId: "G-F476MK2W2C"
+    }
 };
